04f009cf92ce1e53228093e589c4d7918c5cdcda
BinitBudwal <binitsainibudwal@student.mitt.ca>
2022-07-06 18:28:46 -0500

    lab3

---
+++ Controllers/HotelController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IntroductionToLINQandASP.NetMVC_SD115.Models;
+
+namespace IntroductionToLINQandASP.NetMVC_SD115.Controllers
+{
+    public class HotelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AllRooms()
+        {
+            List<Room> allRooms = Hotel.Rooms.ToList();
+            return View(allRooms);
+        }
+    }
+}
